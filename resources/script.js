0b69ab6a87e7eb7282a44bc0bf6e2b6ba957cb42
Calabe <57757790+Unknown0verlord@users.noreply.github.com>
2019-11-18 08:30:39 -0500

    Made pixels clickable

--- resources/script.js
+++ resources/script.js
@@ -1,4 +1,10 @@
 function setPixelColour(pixel) 
 {
-pixel.style.backgroundcolor = 'black';
+pixel.style.backgroundColor = pencolour;
+}
+var pencolour = 'black';
+
+function setPenColour(pen)
+{
+    pencolour = pen;
 }
